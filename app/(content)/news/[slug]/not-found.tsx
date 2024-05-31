a78d26f082540b28533a641fec7fc7e08619ae34
Natalia Zagórska <vermenea@gmail.com>
2024-05-31 16:04:28 +0200

    fix: &apos

--- app/(content)/news/[slug]/not-found.tsx
+++ app/(content)/news/[slug]/not-found.tsx
@@ -2,7 +2,7 @@ export default function NewsNotFound() {
 	return (
 		<div id='error'>
 			<h1>Not Found</h1>
-			<p>Couldn't find requested news</p>
+			<p>Couldnt find requested news</p>
 		</div>
 	);
 }
